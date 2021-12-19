0be8d7dae8cbd946a6b267501f2f291133d97674
Nail' Gafarov <nil20122013@gmail.com>
2021-12-19 20:00:54 +0500

    1, 2, 3 -> a, b, c

--- tests/array.lua
+++ tests/array.lua
@@ -9,17 +9,17 @@ TestArray = {
 	end;
 
 	["test: Should contain \"__data\" table with one element when passing single argument"] = function ()
-		luaunit.assertEquals(array(1).__data, {1});
+		luaunit.assertEquals(array("a").__data, {"a"});
 	end;
 
 	["test: Should consider single argument as a source when passing a plain table"] = function ()
 		luaunit.assertEquals(array({}).__data, {})
-		luaunit.assertEquals(array({1, 2, 3}).__data, {1, 2, 3})
+		luaunit.assertEquals(array({"a", "b", "c"}).__data, {"a", "b", "c"})
 	end;
 
 	["test: Should consider single argument as an element when passing an array"] = function ()
-		local a1 = array(array(1))
-		luaunit.assertEquals(a1.__data[1].__data, {1})
+		local a1 = array(array("a"))
+		luaunit.assertEquals(a1.__data[1].__data, {"a"})
 		luaunit.assertTrue(getmetatable(a1.__data[1]) == getmetatable(a1))
 	end;
 
@@ -34,41 +34,41 @@ TestArray = {
 	end;
 
 	["test: Should contain \"__data\" table with multiple elements when passing multiple arguments"] = function ()
-		luaunit.assertEquals(array(1, 2, 3).__data, {1, 2, 3});
-		luaunit.assertEquals(array({1, 2, 3}).__data, {1, 2, 3});
+		luaunit.assertEquals(array("a", "b", "c").__data, {"a", "b", "c"});
+		luaunit.assertEquals(array({"a", "b", "c"}).__data, {"a", "b", "c"});
 	end;
 
 	["test: Should consider an argument as is when passing a plain table"] = function ()
-		luaunit.assertEquals(array({1, 2, {3}}).__data, {1, 2, {3}})
-		luaunit.assertEquals(array(1, 2, {3}).__data, {1, 2, {3}})
-		luaunit.assertEquals(array({1}, 2, 3).__data, {{1}, 2, 3})
+		luaunit.assertEquals(array({"a", "b", {"c"}}).__data, {"a", "b", {"c"}})
+		luaunit.assertEquals(array("a", "b", {"c"}).__data, {"a", "b", {"c"}})
+		luaunit.assertEquals(array({"a"}, "b", "c").__data, {{"a"}, "b", "c"})
 	end;
 
 	["test: Should not wrap table when passing at as an element"] = function ()
-		luaunit.assertNil(getmetatable(array(1, {2}).__data[2]))
+		luaunit.assertNil(getmetatable(array("a", {"b"}).__data[2]))
 	end;
 
 	["test: Should consider an argument as is when passing an array"] = function ()
-		local a = array(1, 2, array(3))
-		luaunit.assertEquals(a.__data[3].__data, {3})
+		local a = array("a", "b", array("c"))
+		luaunit.assertEquals(a.__data[3].__data, {"c"})
 		luaunit.assertTrue(rawequal(getmetatable(a.__data[3]), getmetatable(a)))
 	end;
 
 	["test: Should consider an argument as is when passing an object"] = function (self)
-		luaunit.assertTrue(rawequal(array(1, 2, self.o).__data[3], self.o));
-		luaunit.assertTrue(rawequal(array({1, 2, self.o}).__data[3], self.o));
+		luaunit.assertTrue(rawequal(array("a", "b", self.o).__data[3], self.o));
+		luaunit.assertTrue(rawequal(array({"a", "b", self.o}).__data[3], self.o));
 	end;
 
 	["test: Should preserve nil when passing it as the first element"] = function ()
-		luaunit.assertEquals(array(nil, 2, 3).__data, {nil, 2, 3});
+		luaunit.assertEquals(array(nil, "b", "c").__data, {nil, "b", "c"});
 	end;
 
 	["test: Should preserve nil when passing it as the middle element"] = function ()
-		luaunit.assertEquals(array(1, nil, 3).__data, {1, nil, 3});
+		luaunit.assertEquals(array("a", nil, "c").__data, {"a", nil, "c"});
 	end;
 
 	["test: Should preserve nil when passing it as the last element"] = function ()
-		luaunit.assertEquals(array(1, 2, nil).__data, {1, 2, nil});
+		luaunit.assertEquals(array("a", "b", nil).__data, {"a", "b", nil});
 	end;
 
 	["test: Should preserve false when passing it as a first value"] = function ()
