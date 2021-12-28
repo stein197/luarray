7892a6c9b2b4fe979a7776262219751ea6b9280e
Nail' Gafarov <nil20122013@gmail.com>
2021-12-28 21:49:10 +0500

    Deleted len test suite

--- tests/len.lua
+++
@@ -1,9 +0,0 @@
-TestLen = {
-	["test: len(): Length of empty table is 0"] = function ()
-		luaunit.assertEquals(array():len(), 0)
-	end;
-
-	["test: len()"] = function ()
-		luaunit.assertEquals(array(1, 2, 3):len(), 3)
-	end;
-}
