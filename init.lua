c772e5e2b90c34eeb5e248dc6cc6a9b0f2e5e160
Nail Gafarov <nil20122013@gmail.com>
2021-12-30 16:33:46 +0500

    Added conversion 0 -> 1

--- init.lua
+++ init.lua
@@ -336,8 +336,8 @@ end
 ---                    relative to the end of the array.
 --- @return array rs Slice of the array.
 function pt:slice(from, to)
-	from = from == nil and 1 or from < 0 and self.__len + from + 1 or from
-	to = to == nil and self.__len or to < 0 and self.__len + to + 1 or to
+	from = from == 0 and 1 or from == nil and 1 or from < 0 and self.__len + from + 1 or from
+	to = to == 0 and 1 or to == nil and self.__len or to < 0 and self.__len + to + 1 or to
 	if to < from then
 		error(string.format("Cannot slice the array from %d to %d index: %d is lesser than %d", from, to, to, from))
 	end
