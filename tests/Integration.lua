ba6b09fde6281b502493c695045b73aa2bf852c7
Nail' Gafarov <nil20122013@gmail.com>
2022-01-14 20:05:22 +0500

    Added simple integrational test

--- tests/Integration.lua
+++ tests/Integration.lua
@@ -1,8 +1,22 @@
--- TODO
 TestIntegration = {
 	test = function ()
-		local a = array("a", "b", "c")
-		a = a:map(function (i, elt) return elt:upper() end)
-		
+		local a = array("a", "b", "c", "d", "e", "f")
+		a[7] = "g"
+		a = a..array("h", "i", "j", "k", "l")
+		a = a * array("a", "b", "c", "d", "e", "f", "g")
+		a = a + array("h", "i", "j", "k", "l")
+		a = a - array("f", "g")
+		a = a:filter(function (i, elt) return 97 <= elt:byte() and elt:byte() <= 102 end):map(function (i, elt) return elt:upper() end):clone():sort(function (a, b)
+			return a > b
+		end)
+		luaunit.assertEquals(a:join(""), "EDCBA")
+		a = a:reverse()
+		a:addend("f")
+		a:addend("A")
+		a = a:uniq()
+		a:del(-2)
+		a = a:chunk(2):flat()
+		a:addbefore(1, "a")
+		luaunit.assertEquals(a:totable(), {"a", "A", "B", "C", "D", "f"})
 	end;
 }
